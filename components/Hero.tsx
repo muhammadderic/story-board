525e7502f9d136545edb5913dc3ff66809f80ae8
muhammadderic <muhammad.donny.e@gmail.com>
2025-01-24 15:19:11 +0700

    Create hero section, add new utilities into globals css, update primary and secondary color theme

---
+++ components/Hero.tsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <section className="primary_container">
+      <h1 className="heading">
+        Tell Your Story, <br />
+        Where Stories Begin
+      </h1>
+
+      <p className="sub-heading !max-w-3xl">
+        Express Your Creativity, Share Your Journey, and Inspire Others.
+      </p>
+    </section >
+  )
+}
+
+export default Hero
