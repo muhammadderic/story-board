1857ef85dcb4aaad2df5caa06bdc7fa65be58b18
muhammadderic <muhammad.donny.e@gmail.com>
2025-01-29 12:54:21 +0700

    Install and configure auth js, add login and logout button in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image"
 import Link from "next/link"
+import { auth, signOut, signIn } from "@/auth"
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth();
   return (
     <header className="px-5 py-3 bg-white shadow-sm">
       <nav className="flex justify-between items-center">
@@ -15,9 +17,31 @@ const Navbar = () => {
         </Link>
 
         <div className="flex items-center gap-5 text-black">
-          <button type="submit">
-            Login
-          </button>
+          {
+            session && session.user ? (
+              <>
+                <form action={async () => {
+                  "use server";
+
+                  await signOut({ redirectTo: "/" });
+                }}>
+                  <button type="submit" className="max-sm:hidden">
+                    Logout
+                  </button>
+                </form>
+              </>
+            ) : (
+              <form action={async () => {
+                "use server";
+
+                await signIn("github");
+              }}>
+                <button type="submit" className="max-sm:hidden">
+                  Login
+                </button>
+              </form>
+            )
+          }
         </div>
       </nav>
 
